89e90b2d0941a5988e34fa2a7cc735585179a088
hilmizul <netspytux@gmail.com>
2021-12-15 07:58:49 +0700

    migrate to sb

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 import router from './router'
 
+import VueSupabase from 'vue-supabase'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
@@ -20,6 +22,11 @@ import {VueSpinners} from '@saeris/vue-spinners'
 
 Vue.config.productionTip = false
 
+Vue.use(VueSupabase, {
+  supabaseUrl: "https://dsryzuznqufyhlnodxyi.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODQ4ODI4MiwiZXhwIjoxOTU0MDY0MjgyfQ.wu8WQnl-kS1Ee5PBkdsBaF0h-VRwzWQZXk69usbwyD4",
+  supabaseOptions: {}
+})
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(NProgress)
