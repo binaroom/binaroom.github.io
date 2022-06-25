07daa80a003548a258fa10f7c6522bd2abeac145
hilmizul <netspytux@gmail.com>
2022-06-25 16:56:48 +0700

    change Size info

--- src/pages/Size.vue
+++ src/pages/Size.vue
@@ -2,11 +2,36 @@
   <section class="binaroom">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
           <div class="mt-5 mb-5" data-aos="fade-up">
             <h1>Size Chart.</h1>
             <div class="mb-3 mt-3">
-              <img src="https://dsryzuznqufyhlnodxyi.supabase.co/storage/v1/object/sign/binarry/gamis/size-gamis?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiaW5hcnJ5L2dhbWlzL3NpemUtZ2FtaXMiLCJpYXQiOjE2NTU0MzAxNzcsImV4cCI6MTk3MDc5MDE3N30.Y4mLJDdhXdn19whf6GBFL_axHAgiVsy2NjFUaQHdhdo" alt="size" class="size" data-aos="zoom-in" data-aos-duration="800" />
+              <table class="table table-bordered table-striped text-center">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>S</th>
+                    <th>M</th>
+                    <th>L</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>LD</th>
+                    <th>95</th>
+                    <th>97</th>
+                    <th>100</th>
+                  </tr>
+                  <tr>
+                    <th>PB</th>
+                    <th>125</th>
+                    <th>130</th>
+                    <th>135</th>
+                  </tr>
+                </tbody>
+              </table>
+              <em>*dalam satuan centimeter (cm)</em>
+              <!-- <img src="https://dsryzuznqufyhlnodxyi.supabase.co/storage/v1/object/sign/binarry/gamis/size-gamis?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiaW5hcnJ5L2dhbWlzL3NpemUtZ2FtaXMiLCJpYXQiOjE2NTU0MzAxNzcsImV4cCI6MTk3MDc5MDE3N30.Y4mLJDdhXdn19whf6GBFL_axHAgiVsy2NjFUaQHdhdo" alt="size" class="size" data-aos="zoom-in" data-aos-duration="800" /> -->
             </div>
           </div>
         </div>
@@ -26,6 +51,9 @@ export default {
 </script>
 
 <style scoped>
+table {
+  font-size: 3vh;
+}
 .binaroom {
   width: 100%;
   height: auto;
